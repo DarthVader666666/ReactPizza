29e37c037fb715ae497f99f0ee67bfb9740b094a
alinagunich2 <gunich.alina@gmail.com>
2024-05-27 10:28:02 +0300

    finished html scss components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((categor, index) => (
+          <li
+            onClick={() => {
+              setActiveIndex(index);
+            }}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {categor}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
